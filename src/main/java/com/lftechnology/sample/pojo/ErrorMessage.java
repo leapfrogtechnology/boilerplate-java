b527f4210e6c82e543b1c63f8d3c99502396df96
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2017-05-12 17:23:14 +0545

    JB-2 Add error message pojo

---
+++ src/main/java/com/lftechnology/sample/pojo/ErrorMessage.java
@@ -0,0 +1,24 @@
+package com.lftechnology.sample.pojo;
+
+public class ErrorMessage {
+
+    private Integer status;
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+}
