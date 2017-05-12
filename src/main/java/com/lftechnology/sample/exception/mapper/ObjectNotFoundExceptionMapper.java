83d7f21778f9291c290ad6aa95818873dffdab38
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2017-05-12 17:22:55 +0545

    JB-2 Add Object not found exception mapper

--- src/main/java/com/lftechnology/sample/exception/mapper/ObjectNotFoundExceptionMapper.java
+++ src/main/java/com/lftechnology/sample/exception/mapper/ObjectNotFoundExceptionMapper.java
@@ -1,12 +1,26 @@
 package com.lftechnology.sample.exception.mapper;
 
-import org.hibernate.transaction.synchronization.ExceptionMapper;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import com.lftechnology.sample.exception.ObjectNotFoundException;
+import com.lftechnology.sample.pojo.ErrorMessage;
 
 /**
  * Exception mapper for {@link ObjectNotFoundException}
  * 
  * @author Achyut Pokhrel <achyutpokhrel@lftechnology.com>
  */
-public class ObjectNotFoundExceptionMapper implements ExceptionMapper {
+@Provider
+public class ObjectNotFoundExceptionMapper implements ExceptionMapper<ObjectNotFoundException> {
 
+    @Override
+    public Response toResponse(ObjectNotFoundException exception) {
+        ErrorMessage errorMessage = new ErrorMessage();
+        errorMessage.setMessage(exception.getMessage());
+        errorMessage.setStatus(Status.NOT_FOUND.getStatusCode());
+        return Response.status(Status.NOT_FOUND).entity(errorMessage).build();
+    }
 }
