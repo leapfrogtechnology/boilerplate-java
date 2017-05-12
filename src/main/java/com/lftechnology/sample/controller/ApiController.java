581613de2a5f4976c4cb78a95aa80c95c5945442
Sanish Maharjan <sanishmaharjan@lftechnology.com>
2017-05-12 17:10:08 +0545

    basic api controller added

---
+++ src/main/java/com/lftechnology/sample/controller/ApiController.java
@@ -0,0 +1,42 @@
+package com.lftechnology.sample.controller;
+
+import com.lftechnology.sample.exception.ApiException;
+import com.lftechnology.sample.model.User;
+import com.lftechnology.sample.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/user/{id}")
+    public HashMap<String, Object> userinfo(@PathVariable Long id) throws ApiException {
+        HashMap<String, Object> response = new HashMap<>();
+        User userInfo = userService.findUserById(id);
+        if (userInfo != null) {
+            response.put("data", userInfo);
+            response.put("statusCode", HttpStatus.OK.value());
+            return response;
+        } else {
+            throw new ApiException("Person not exist", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/user")
+    public HashMap<String, Object> addPerson(@RequestParam(value = "name", required = true) String name,
+            @RequestParam(value = "country", required = false) String country) {
+        HashMap<String, Object> response = new HashMap<>();
+        return null;
+    }
+}
