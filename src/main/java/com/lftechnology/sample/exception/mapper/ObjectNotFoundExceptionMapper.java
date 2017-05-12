cca052f8f34f513c0d0ee6f5cd83b36875925071
Achyut Pokhrel <achyutpokhrel@lftechnology.com>
2017-05-12 16:53:43 +0545

    Add exception

---
+++ src/main/java/com/lftechnology/sample/exception/mapper/ObjectNotFoundExceptionMapper.java
@@ -0,0 +1,12 @@
+package com.lftechnology.sample.exception.mapper;
+
+import org.hibernate.transaction.synchronization.ExceptionMapper;
+
+/**
+ * Exception mapper for {@link ObjectNotFoundException}
+ * 
+ * @author Achyut Pokhrel <achyutpokhrel@lftechnology.com>
+ */
+public class ObjectNotFoundExceptionMapper implements ExceptionMapper {
+
+}
